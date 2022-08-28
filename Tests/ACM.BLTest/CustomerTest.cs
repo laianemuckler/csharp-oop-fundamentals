8f1b046af0447e15e6e6099be510aa796df19c16
Laiane Muckler <laianecm@gmail.com>
2022-08-28 13:45:20 -0300

    first commit

---
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,62 @@
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestValid()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins, Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameFirstNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameLastNameEmpty()
+        {
+            //-- Arrange
+            Customer customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+
+            string expected = "Bilbo";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //-- Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
